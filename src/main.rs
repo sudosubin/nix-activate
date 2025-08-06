c55ba1933ea9ba0c37b7c5a62a6f292cfc1d8f35
sudosubin <sudosubin@gmail.com>
2025-08-06 20:03:03 +0900

    refactor: update code and performance optimizations

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use std::io::Write;
 use std::path::{Path, PathBuf};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let current_dir = std::env::current_dir()?;
+    let current_dir = env::current_dir()?;
     let envrc_path = current_dir.join(".envrc");
 
     // check current directory's flake
@@ -18,7 +18,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     let nix_activate_root = match env::var("NIX_ACTIVATE_ROOT") {
-        Ok(var) if PathBuf::from(&var).is_dir() => {
+        Ok(var) if Path::new(&var).is_dir() => {
             info!("try external flakes from {}", var);
             var
         }
@@ -50,43 +50,35 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .and_then(|url| GitUrl::parse(&url).ok());
 
             if let Some(url) = url {
-                let host = url.host.unwrap_or("_".to_string());
+                let host = url.host.as_deref().unwrap_or("_");
                 let path =
-                    PathBuf::from_iter([&nix_activate_root, &host, &url.fullname, relative_path]);
+                    PathBuf::from_iter([&nix_activate_root, host, &url.fullname, relative_path]);
 
                 if path.join("flake.nix").exists() {
-                    return write_use_flake(&envrc_path, path.to_str().unwrap());
+                    if let Some(path_str) = path.to_str() {
+                        return write_use_flake(&envrc_path, path_str);
+                    }
                 }
             }
         }
     }
 
     // check local-only flake
-    let current_dir_str = current_dir.to_str().unwrap();
-    let fullname = current_dir_str
-        .split(std::path::MAIN_SEPARATOR_STR)
-        .collect::<Vec<_>>()
-        .iter()
-        .rev()
-        .take(3)
-        .rev()
-        .cloned()
-        .collect::<Vec<_>>()
-        .join(std::path::MAIN_SEPARATOR_STR);
-
-    let path = PathBuf::from_iter([&nix_activate_root, &fullname]);
+    let components: Vec<_> = current_dir.components().rev().take(3).collect();
+    let fullname = components.into_iter().rev().collect::<PathBuf>();
+    let path = PathBuf::from_iter([Path::new(&nix_activate_root), &fullname]);
     if path.join("flake.nix").exists() {
         return write_use_flake(&envrc_path, path.to_str().unwrap());
     }
 
     warn!("no flake found");
-    return Ok(());
+    Ok(())
 }
 
 fn write_use_flake(path: &Path, flake: &str) -> Result<(), Box<dyn std::error::Error>> {
     info!("use flake {}", flake);
     let mut file = File::create(path)?;
-    match file.write_all(format!("use flake {}", flake).as_bytes()) {
+    match write!(file, "use flake {}", flake) {
         Ok(_) => Ok(()),
         Err(e) => {
             error!("failed to write flake to {}", path.display());
